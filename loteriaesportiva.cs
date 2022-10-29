0bd8616af80d9fdb6f1342e81421c4b86035ef1e
rosealiz <112977051+rosealiz@users.noreply.github.com>
2022-10-29 18:06:59 -0300

    Update loteriaesportiva.cs

--- loteriaesportiva.cs
+++ loteriaesportiva.cs
@@ -6,58 +6,58 @@ public class HelloWorld
     {
       int i, j;
       int pontuacao = 0;
-      string[,] resultadooficial = new string[13, 3];
-      resultadooficial[0,0] = "1";
-      resultadooficial[0,1] = "0";
-      resultadooficial[0,2] = "0";
+      int[,] resultadooficial = new int[13, 3];
+      resultadooficial[0,0] = int.Parse("1");
+      resultadooficial[0,1] = int.Parse("0");
+      resultadooficial[0,2] = int.Parse("0");
       
-      resultadooficial[1,0] = "0";
-      resultadooficial[1,1] = "1";
-      resultadooficial[1,2] = "0";
+      resultadooficial[1,0] = int.Parse("0");
+      resultadooficial[1,1] = int.Parse("1");
+      resultadooficial[1,2] = int.Parse("0");
       
-      resultadooficial[2,0] = "0";
-      resultadooficial[2,1] = "1";
-      resultadooficial[2,2] = "0";
+      resultadooficial[2,0] = int.Parse("0");
+      resultadooficial[2,1] = int.Parse("1");
+      resultadooficial[2,2] = int.Parse("0");
       
-      resultadooficial[3,0] = "0";
-      resultadooficial[3,1] = "0";
-      resultadooficial[3,2] = "1";
+      resultadooficial[3,0] = int.Parse("0");
+      resultadooficial[3,1] = int.Parse("0");
+      resultadooficial[3,2] = int.Parse("1");
       
-      resultadooficial[4,0] = "1";
-      resultadooficial[4,1] = "0";
-      resultadooficial[4,2] = "0";
+      resultadooficial[4,0] = int.Parse("1");
+      resultadooficial[4,1] = int.Parse("0");
+      resultadooficial[4,2] = int.Parse("0");
       
-      resultadooficial[5,0] = "1";
-      resultadooficial[5,1] = "0";
-      resultadooficial[5,2] = "0";
+      resultadooficial[5,0] = int.Parse("1");
+      resultadooficial[5,1] = int.Parse("0");
+      resultadooficial[5,2] = int.Parse("0");
       
-      resultadooficial[6,0] = "0";
-      resultadooficial[6,1] = "0";
-      resultadooficial[6,2] = "1";
+      resultadooficial[6,0] = int.Parse("0");
+      resultadooficial[6,1] = int.Parse("0");
+      resultadooficial[6,2] = int.Parse("1");
       
-      resultadooficial[7,0] = "0";
-      resultadooficial[7,1] = "1";
-      resultadooficial[7,2] = "0";
+      resultadooficial[7,0] = int.Parse("0");
+      resultadooficial[7,1] = int.Parse("1");
+      resultadooficial[7,2] = int.Parse("0");
       
-      resultadooficial[8,0] = "0";
-      resultadooficial[8,1] = "0";
-      resultadooficial[8,2] = "1";
+      resultadooficial[8,0] = int.Parse("0");
+      resultadooficial[8,1] = int.Parse("0");
+      resultadooficial[8,2] = int.Parse("1");
       
-      resultadooficial[9,0] = "1";
-      resultadooficial[9,1] = "0";
-      resultadooficial[9,2] = "0";
+      resultadooficial[9,0] = int.Parse("1");
+      resultadooficial[9,1] = int.Parse("0");
+      resultadooficial[9,2] = int.Parse("0");
       
-      resultadooficial[10,0] = "0";
-      resultadooficial[10,1] = "0";
-      resultadooficial[10,2] = "1";
+      resultadooficial[10,0] = int.Parse("0");
+      resultadooficial[10,1] = int.Parse("0");
+      resultadooficial[10,2] = int.Parse("1");
       
-      resultadooficial[11,0] = "0";
-      resultadooficial[11,1] = "0";
-      resultadooficial[11,2] = "1";
+      resultadooficial[11,0] = int.Parse("0");
+      resultadooficial[11,1] = int.Parse("0");
+      resultadooficial[11,2] = int.Parse("1");
       
-      resultadooficial[12,0] = "1";
-      resultadooficial[12,1] = "0";
-      resultadooficial[12,2] = "0";
+      resultadooficial[12,0] = int.Parse("1");
+      resultadooficial[12,1] = int.Parse("0");
+      resultadooficial[12,2] = int.Parse("0");
       
       
         int[,] apostas = new int[13, 3];
